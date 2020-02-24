93372518eaef3c6228b5615a90d6d5fa42f50258
Edik Deineka <e.v.deineka@gmail.com>
2020-02-24 18:03:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+function push(arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    arr[arr.length] = callback(arr[i], i, arr);
+  }
+};
+push(arr, callback);
+
+function callback() {}
+
+
+
+function forEach(arr, callback) {
+    for (var i = 0; i < arr.length; i++) {
+      callback(arr[i], i, arr);
+    }
+  };
+forEach(arr, callback);
+
+function callback() {}
+
+
+
+function map(arr, callback) {
+  var newMap = [];
+  for (var i = 0; i < arr.length; i++) {
+    newMap.push(callback(arr[i], i, arr));
+  }
+  return newMap;
+};
+map(arr, callback);
+console.log(newMap);
+
+function callback() {}
+
+
+
+function some(arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      return true;
+    }
+  }
+  return false;
+};
+some(arr, callback);
+
+function callback() {}
+
+
+
+function every(arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      return false;
+    }
+  }
+  return true;
+};
+every(arr, callback);
+
+function callback() {}
+
+
+
+function filter(arr, callback) {
+  var newfilter = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      newfilter.push(callback(arr[i], i, arr));
+    }
+  }
+  return newfilter;
+};
+filter(arr, callback);
+console.log(newfilter);
+
+function callback() {}
